585c7d8121e3c6945e07d893e3c31b1b07111d48
iamsujun <iamsujun@gmail.com>
2019-11-21 11:36:36 +0800

    Update prepend.php

--- prepend.php
+++ prepend.php
@@ -3,15 +3,19 @@ require_once __DIR__.'/vendor/autoload.php';
 
 use SebastianBergmann\CodeCoverage\CodeCoverage;
 
-$coverage = new CodeCoverage;
-//白名单配置，具体需要收集的目录
-$coverage->filter()->addDirectoryToWhitelist(dirname($_SERVER['DOCUMENT_ROOT']).'/app');
+$env = parse_ini_file(__DIR__ . "/.env");
 
-$coverage->start($_SERVER['REQUEST_URI'] ?? "coverage");
-
-register_shutdown_function(function (CodeCoverage $coverage){
-    $coverage->stop();
-    $cov = '<?php return unserialize('.var_export(serialize($coverage), true).');';
-    file_put_contents(__DIR__.'/storage/coverage/site.'.date('U').'.'.uniqid().'.cov', $cov);
-    //system(__DIR__."/vendor/bin/phpcov merge --html=".__DIR__."/storage/coverage_html ".__DIR__."/storage/coverage");
-}, $coverage);
+if(isset($env['coverage_enable']) && $env['coverage_enable']) {
+    $coverage = new CodeCoverage;
+    //白名单配置，具体需要收集的目录
+    $coverage->filter()->addDirectoryToWhitelist(dirname($_SERVER['DOCUMENT_ROOT']).'/app');
+    
+    $coverage->start($_SERVER['REQUEST_URI'] ?? "coverage");
+    
+    register_shutdown_function(function (CodeCoverage $coverage){
+        $coverage->stop();
+        $cov = '<?php return unserialize('.var_export(serialize($coverage), true).');';
+        file_put_contents(__DIR__.'/storage/coverage/site.'.date('U').'.'.uniqid().'.cov', $cov);
+        //system(__DIR__."/vendor/bin/phpcov merge --html=".__DIR__."/storage/coverage_html ".__DIR__."/storage/coverage");
+    }, $coverage);
+}
