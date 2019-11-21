95eb7d034df1fda099753e40d8299732f7cf6abf
iamsujun <iamsujun@gmail.com>
2019-11-21 09:32:54 +0800

    Update prepend.php

--- prepend.php
+++ prepend.php
@@ -13,5 +13,5 @@ register_shutdown_function(function (CodeCoverage $coverage){
     $coverage->stop();
     $cov = '<?php return unserialize('.var_export(serialize($coverage), true).');';
     file_put_contents(__DIR__.'/storage/coverage/site.'.date('U').'.'.uniqid().'.cov', $cov);
-    system(__DIR__."/vendor/bin/phpcov merge --html=".__DIR__."/storage/coverage_html ".__DIR__."/storage/coverage");
+    //system(__DIR__."/vendor/bin/phpcov merge --html=".__DIR__."/storage/coverage_html ".__DIR__."/storage/coverage");
 }, $coverage);
